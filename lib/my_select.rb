f2f13256d90340cd2f475af0c8ff1829c9001134
Kian Edalat <akedalat@gmail.com>
2019-01-13 21:30:58 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,9 +1,12 @@
 def my_select(collection)
 i = 0
 new_collection = []
+
 while i < collection.length
+  if collection[i] = true
   new_collection.push(yield(collection[i]))
   i += 1
+  end
 
 end
 new_collection
